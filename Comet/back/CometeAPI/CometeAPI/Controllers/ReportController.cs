f07b23301afec03be6cc7c0ef29d43933de1ec12
Sébastien Gaillard <82144644+SebGaillard1@users.noreply.github.com>
2024-06-20 18:25:14 +0200

    Add ReportController.cs

---
+++ Comet/back/CometeAPI/CometeAPI/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CometeAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReportController : ControllerBase
+{
+    [HttpPost("generate")]
+    public async Task<IActionResult> Generate()
+    {
+        /*if (file == null || file.Length == 0)
+            return BadRequest("No file uploaded.");
+
+        var text = await _fileService.ExtractTextAsync(file);
+        var response = await _fileService.ProcessTextWithOpenAIAsync(text);*/
+        return Ok();
+    }
+}
