03ebeccccda1bbddbfc73b2bd389953ccb3dc56e
matteo <matnoss@gmail.com>
2024-06-20 18:45:15 +0200

    feat: on test des truc avec primeng

---
+++ Front/src/components/folder-table/folder-table.component.ts
@@ -0,0 +1,184 @@
+import { Component, OnInit } from '@angular/core';
+import { ImportsModule } from './imports';
+import { Customer, Representative } from '@domain/customer';
+import { CustomerService } from '@service/customerservice';
+@Component({
+  selector: 'app-folder-table',
+  standalone: true,
+  imports: [ImportsModule],
+  providers: [CustomerService],
+  templateUrl: './folder-table.component.html',
+  styleUrl: './folder-table.component.css',
+  styles: [
+    `
+    :host ::ng-deep {
+        .p-paginator {
+            .p-paginator-current {
+                margin-left: auto;
+            }
+        }
+
+        .p-progressbar {
+            height: .5rem;
+            background-color: #D8DADC;
+
+            .p-progressbar-value {
+                background-color: #607D8B;
+            }
+        }
+
+        .table-header {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .p-calendar .p-datepicker {
+            min-width: 25rem;
+
+            td {
+                font-weight: 400;
+            }
+        }
+
+        .p-datatable.p-datatable-customers {
+            .p-datatable-header {
+                padding: 1rem;
+                text-align: left;
+                font-size: 1.5rem;
+            }
+
+            .p-paginator {
+                padding: 1rem;
+            }
+
+            .p-datatable-thead > tr > th {
+                text-align: left;
+            }
+
+            .p-datatable-tbody > tr > td {
+                cursor: auto;
+            }
+
+            .p-dropdown-label:not(.p-placeholder) {
+                text-transform: uppercase;
+            }
+        }
+
+        .p-w-100 {
+            width: 100%;
+        }
+
+        /* Responsive */
+        .p-datatable-customers .p-datatable-tbody > tr > td .p-column-title {
+            display: none;
+        }
+    }
+
+    @media screen and (max-width: 960px) {
+        :host ::ng-deep {
+            .p-datatable {
+                &.p-datatable-customers {
+                    .p-datatable-thead > tr > th,
+                    .p-datatable-tfoot > tr > td {
+                        display: none !important;
+                    }
+
+                    .p-datatable-tbody > tr {
+                        border-bottom: 1px solid var(--layer-2);
+
+                        > td {
+                            text-align: left;
+                            width: 100%;
+                            display: flex;
+                            align-items: center;
+                            border: 0 none;
+
+                            .p-column-title {
+                                min-width: 30%;
+                                display: inline-block;
+                                font-weight: bold;
+                            }
+
+                            p-progressbar {
+                                width: 100%;
+                            }
+
+                            &:last-child {
+                                border-bottom: 1px solid var(--surface-d);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    `
+  ],
+})
+
+
+export class FolderTableComponent implements OnInit{
+  customers!: Customer[];
+
+  selectedCustomers!: Customer[];
+
+  representatives!: Representative[];
+
+  statuses!: any[];
+
+  loading: boolean = true;
+
+  activityValues: number[] = [0, 100];
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+    this.customerService.getCustomersLarge().then((customers) => {
+      this.customers = customers;
+      this.loading = false;
+
+      this.customers.forEach((customer) => (customer.date = new Date(<Date>customer.date)));
+    });
+
+    this.representatives = [
+      { name: 'Amy Elsner', image: 'amyelsner.png' },
+      { name: 'Anna Fali', image: 'annafali.png' },
+      { name: 'Asiya Javayant', image: 'asiyajavayant.png' },
+      { name: 'Bernardo Dominic', image: 'bernardodominic.png' },
+      { name: 'Elwin Sharvill', image: 'elwinsharvill.png' },
+      { name: 'Ioni Bowcher', image: 'ionibowcher.png' },
+      { name: 'Ivan Magalhaes', image: 'ivanmagalhaes.png' },
+      { name: 'Onyama Limba', image: 'onyamalimba.png' },
+      { name: 'Stephen Shaw', image: 'stephenshaw.png' },
+      { name: 'Xuxue Feng', image: 'xuxuefeng.png' }
+    ];
+
+    this.statuses = [
+      { label: 'Unqualified', value: 'unqualified' },
+      { label: 'Qualified', value: 'qualified' },
+      { label: 'New', value: 'new' },
+      { label: 'Negotiation', value: 'negotiation' },
+      { label: 'Renewal', value: 'renewal' },
+      { label: 'Proposal', value: 'proposal' }
+    ];
+  }
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'unqualified':
+        return 'danger';
+
+      case 'qualified':
+        return 'success';
+
+      case 'new':
+        return 'info';
+
+      case 'negotiation':
+        return 'warning';
+
+      case 'renewal':
+        return null;
+    }
+  }
+}
