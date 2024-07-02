8f7b705fff57acc49521d682601f4989c7c5a867
Matteo-Nossro <matnoss@gmail.com>
2024-07-02 19:21:48 +0200

    feat(Pages): Ajout du squelette des pages

---
+++ Front/src/pages/profil-page/profil-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './profil-page.component.html',
+  styleUrl: './profil-page.component.css'
+})
+export class ProfilPageComponent {
+
+}
